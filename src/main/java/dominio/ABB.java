b82817a1fc4262825e81daee18b9ebb4b3d36e89
Ronald <rl8506@gmail.com>
2022-09-17 19:32:47 -0300

    clase ABB

---
+++ src/main/java/dominio/ABB.java
@@ -0,0 +1,149 @@
+package dominio;
+
+public class ABB<T extends Comparable<T>> {
+
+    private NodoGenArbol raiz;
+
+    public ABB() {
+        this.raiz = null;
+    }
+
+    // Pos: Inserta el dato pasado como parametro en el arbol manteniendolo ordenado.
+    public void insertar(T dato) {
+        if (this.raiz == null) {
+            this.raiz = new NodoGenArbol(dato);
+        } else {
+            insertarRec(this.raiz, dato);
+        }
+    }
+
+    private void insertarRec(NodoGenArbol<Jugador> nodo, T dato) {
+        if (dato.compareTo((T) nodo.getDato())<0) {
+            if (nodo.getIzq() == null) {
+                nodo.setIzq(new NodoGenArbol(dato));
+            } else {
+                insertarRec(nodo.getIzq(), dato);
+            }
+        } else if (dato.compareTo((T) nodo.getDato())>0) {
+            if (nodo.getDer() == null) {
+                nodo.setDer(new NodoGenArbol(dato));
+            } else {
+                insertarRec(nodo.getDer(), dato);
+            }
+        }
+
+    }
+
+    /*
+    //In Order
+    public void listarAscendente(){
+        listarAscendente(this.raiz);
+    }
+
+    private void listarAscendente(NodoGenArbol nodo) {
+        if(nodo != null){// se puede dejar vacio  si nodo ==null
+            listarAscendente(nodo.getIzq());
+            System.out.println(nodo.getDato());
+            listarAscendente(nodo.getDer());
+        }
+    }
+
+    public void listarDescendente(){
+        listarDescendente(this.raiz);
+    }
+
+    private void listarDescendente(NodoGenArbol nodo) {
+        if(nodo != null){// se puede dejar vacio  si nodo ==null
+
+            listarDescendente(nodo.getDer());
+            System.out.println(nodo.getDato());
+            listarDescendente(nodo.getIzq());
+        }
+    }
+
+    //Post Orden
+    public void listarPostOrden(){
+        listarPostOrden(this.raiz);
+    }
+
+    private void listarPostOrden(NodoGenArbol nodo) {
+        if(nodo != null){// se puede dejar vacio  si nodo ==null
+
+            listarPostOrden(nodo.getDer());
+            System.out.println(nodo.getDato());
+            listarPostOrden(nodo.getIzq());
+        }
+    }
+
+    public boolean pertenece(int x){
+        return pertenece(x,raiz);
+    }
+
+    private boolean pertenece(int x, NodoGenArbol nodo) {
+        if(nodo == null){
+            return false;
+        }else if(x == nodo.getDato()){
+            return true;
+        }else{
+            if(x < nodo.getDato()){
+                return pertenece(x,nodo.getIzq());
+            }else{
+                return pertenece(x,nodo.getDer());
+            }
+        }
+    }
+
+    public int borrarMinimo(){
+        return borrarMinimo(raiz,0,null);
+    }
+
+    private int borrarMinimo(NodoGenArbol nodo,int minimo,NodoGenArbol ant)
+    {
+       if(nodo != null) {
+           if (nodo.getIzq() != null) {
+               ant = nodo;
+               int min = nodo.getDato();
+               minimo = min;
+               int minIzq = borrarMinimo(nodo.getIzq(), min, ant);
+               return minIzq;
+           } else {
+               if (ant.getDer() != null) {
+                   ant.setIzq(nodo.getDer());
+                   return nodo.getDato();
+               } else {
+                   ant.setIzq(null);
+                   return nodo.getDato();
+               }
+           }
+       }else{
+           return -1;
+       }
+    }
+     */
+
+    /* Ejercicio 6 */
+    /*Desarrolle un algoritmo que, recibiendo un valor entero k, retorne la cantidad de elementos que son mayores a k.*/
+   /* public int cantidadMayores(int k){
+        return cantidadMayores(k,raiz);
+    }
+
+    private int cantidadMayores(int k,NodoGenArbol nodo) {
+
+        if(nodo!=null){
+           if(k< nodo.getDato()){
+                int derCant = 1+cantidadMayores(k,nodo.getDer());
+                int izqCant = cantidadMayores(k,nodo.getIzq());
+                return derCant+izqCant;
+           }else{
+               return cantidadMayores(k,nodo.getDer());
+           }
+        }else{
+            return 0;
+        }
+    }
+    */
+
+
+
+
+}
